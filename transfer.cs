5a90e4045f7c80f3df75ce27936a6486bbde1e58
Maciej Walanus <mwalanus@outlook.com>
2025-08-21 12:28:47 +0200

    'parts'

--- transfer.cs
+++ transfer.cs
@@ -4,11 +4,30 @@ using System.Text;
 
 var inputFile = args[0];
 var outputFile = args[0] + ".txt";
+var partSize = 49 * 1024 * 1024;
 
 Console.WriteLine($"Input: {inputFile}, Output: {outputFile}");
 
 var content = File.ReadAllBytes(inputFile);
 var base64arr = Convert.ToBase64String(content);
-File.WriteAllText(outputFile, base64arr, Encoding.ASCII);
+if (base64arr.Length > partSize) // assume 1B/1char
+{
+    Console.WriteLine("Writing parts...");
+    var leftover = base64arr.Length;
+    var parts = (int)Math.Ceiling(base64arr.Length / (double)partSize);
+    for (int i = 0; i < parts; i++)
+    {
+        var spanSize = leftover >= partSize ? partSize : leftover;
+        File.WriteAllText($"{outputFile}.{i}.part", base64arr.AsSpan(partSize * i, spanSize));
+        leftover -= partSize;
+        //File.WriteAllText(outputFile, base64arr, Encoding.ASCII);
+    }
+    // split
+}
+else
+{
+    Console.WriteLine("Writing single file...");
+    File.WriteAllText(outputFile, base64arr, Encoding.ASCII);
+}
 
 Console.WriteLine("Finished");
--- transfer_back.cs
+++ transfer_back.cs
@@ -1,14 +1,52 @@
 #!/usr/local/share/dotnet/dotnet run
 
-using System.Text;
-
 var inputFile = args[0];
-var outputFile = args[0].EndsWith(".txt", StringComparison.OrdinalIgnoreCase) ? args[0].Substring(0, args[0].LastIndexOf(".txt", StringComparison.OrdinalIgnoreCase)) : args[0] + ".org";
+var outputFile = args[0];
 
 Console.WriteLine($"Input: {inputFile}, Output: {outputFile}");
 
-var content = File.ReadAllText(inputFile);
+var content = "";
+if (inputFile.EndsWith(".part", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine("Reading parts...");
+
+    outputFile = TrimLast(outputFile, ".part");
+
+    var justName = Path.GetFileName(inputFile);
+    var dir = Path.GetDirectoryName(inputFile);
+
+    var nameParts = justName.Split('.');
+
+    var number = nameParts[^2];
+    var originalName = string.Join('.', nameParts[0..^2]);
+
+    for (int i = 0; ; i++)
+    {
+        var partPath = Path.Join(dir, $"{originalName}.{i}.part");
+        if (File.Exists(partPath))
+        {
+            content += File.ReadAllText(partPath);
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Reading single file...");
+    content = File.ReadAllText(inputFile);
+}
+
+outputFile = outputFile.EndsWith(".txt", StringComparison.OrdinalIgnoreCase) ? TrimLast(outputFile, ".txt") : outputFile + ".org";
+
 var binary = Convert.FromBase64String(content);
 File.WriteAllBytes(outputFile, binary);
 
 Console.WriteLine("Finished");
+
+static string TrimLast(string source, string input)
+{
+    return source.Substring(0, source.LastIndexOf(input, StringComparison.OrdinalIgnoreCase));
+}
