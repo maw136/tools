ca7688053a6eab9996589f06063f20e4204a20ad
Maciej Walanus <mwalanus@outlook.com>
2025-10-07 14:41:46 +0200

    add cert

---
+++ cert.cs
@@ -0,0 +1,52 @@
+public static void Main()
+    {
+        var root = GenerateCertificate("CN=RootCA", null, null, true);
+        var intermediate = GenerateCertificate("CN=IntermediateCA", root.Item1, root.Item2, true);
+        var leaf = GenerateCertificate("CN=LeafCert", intermediate.Item1, intermediate.Item2, false);
+
+        Console.WriteLine("Root CA (Base64):\n" + ToBase64(root));
+        Console.WriteLine("Intermediate CA (Base64):\n" + ToBase64(intermediate));
+        Console.WriteLine("Leaf Cert (Base64):\n" + ToBase64(leaf));
+    }
+
+    private static Tuple<X509Certificate, AsymmetricKeyParameter> GenerateCertificate(
+        string subjectName,
+        X509Certificate issuerCert,
+        AsymmetricKeyParameter issuerKey,
+        bool isCA)
+    {
+        var random = new SecureRandom(new CryptoApiRandomGenerator());
+        var keyGen = new RsaKeyPairGenerator();
+        keyGen.Init(new KeyGenerationParameters(random, 2048));
+        var keyPair = keyGen.GenerateKeyPair();
+
+        var certGen = new X509V3CertificateGenerator();
+        var subjectDN = new X509Name(subjectName);
+        var issuerDN = issuerCert != null ? issuerCert.SubjectDN : subjectDN;
+
+        certGen.SetSerialNumber(BigInteger.ProbablePrime(120, random));
+        certGen.SetIssuerDN(issuerDN);
+        certGen.SetSubjectDN(subjectDN);
+        certGen.SetNotBefore(DateTime.UtcNow.Date);
+        certGen.SetNotAfter(DateTime.UtcNow.Date.AddYears(1));
+        certGen.SetPublicKey(keyPair.Public);
+        certGen.AddExtension(X509Extensions.BasicConstraints, true, new BasicConstraints(isCA));
+
+        var signer = new Asn1SignatureFactory("SHA256WithRSA", issuerKey ?? keyPair.Private, random);
+        var cert = certGen.Generate(signer);
+
+        return Tuple.Create(cert, keyPair.Private);
+    }
+
+    private static string ToBase64(Tuple<X509Certificate, AsymmetricKeyParameter> certAndKey)
+    {
+        var sb = new StringBuilder();
+        using (var sw = new StringWriter(sb))
+        {
+            var pemWriter = new PemWriter(sw);
+            pemWriter.WriteObject(certAndKey.Item1);
+            pemWriter.WriteObject(certAndKey.Item2);
+        }
+        var bytes = Encoding.UTF8.GetBytes(sb.ToString());
+        return Convert.ToBase64String(bytes);
+    }
